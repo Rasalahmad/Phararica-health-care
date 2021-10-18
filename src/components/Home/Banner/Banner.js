ff546abd8dd360ef487b805b8203210b8e8a2f4b
Rasalahmad <rasalahmadsit@gmail.com>
2021-10-18 11:16:50 +0600

    first commit

---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Banner.css';
+import banner1 from '../../../images/Banner/banner1.png'
+import banner2 from '../../../images/Banner/banner2.png'
+import banner3 from '../../../images/Banner/banner3.png'
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className = 'text-end'>
+                        <h3>Medicines cure diseases </h3> <h3> but only doctors can cure patients</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <Carousel.Caption className = 'text-start'>
+                        <h3>People pay the doctor for his trouble</h3>
+                        <h3>for his kindness they still remain in his debt</h3>
+                    </Carousel.Caption>
+
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption className = 'text-end'>
+                        <h3>Our job is the improving the quality of life</h3>
+                        <h3>Not just delaying death</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+
+const Header = () => {
+    return (
+        <>
+             <Navbar bg="dark" variant="dark" sticky='top' collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">PAHARICA HOSPITAL</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/services">Services</Nav.Link>
+                        <Nav.Link as={HashLink} to="/about">About US</Nav.Link>
+                        <Nav.Link as={HashLink} to="/team">Our Team</Nav.Link>
+                        
+                            <Button variant = 'light'>LogOut</Button>
+                            <Nav.Link  as={Link} to="/login">Sign up</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                         
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">user</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
